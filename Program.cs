43db232b1e09af61bb6f700faf0b03e388b1916d
nduvho <nduvhomaguwada@gmail.com>
2025-04-20 19:01:53 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllers();
+
+
+app.Run();
