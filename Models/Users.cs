50a032ed96b6ef9ee24b4e924336dcbc7871f0a9
nduvho <nduvhomaguwada@gmail.com>
2025-04-20 19:07:08 +0200

    added user model

---
+++ Models/Users.cs
@@ -0,0 +1,9 @@
+namespace api.Models{
+    public class User {
+        public string Id { get; set;}
+        public string Name { get; set;}
+        public string Surname { get; set;}
+        public int ContactNo { get; set;}
+        
+    }
+}
