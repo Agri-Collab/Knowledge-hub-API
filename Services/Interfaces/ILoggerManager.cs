fe6d8bcb63c237be2fce6680c38c445f829a7a91
nduvho <nduvhomaguwada@gmail.com>
2025-05-03 11:41:11 +0200

    implemented logging services

---
+++ Services/Interfaces/ILoggerManager.cs
@@ -0,0 +1,10 @@
+namespace api.Services.Interfaces
+{
+    public interface ILoggerManager
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+    }
+}
