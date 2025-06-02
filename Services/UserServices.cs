cbc8db59160864b1268b0ca12ae7e85644792ab2
nduvho <nduvhomaguwada@gmail.com>
2025-06-02 21:39:01 +0200

    implemented user controller

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -1,3 +1,4 @@
+using api.Models;
 using api.Repository;
 using api.Services.Interfaces;
 
@@ -12,6 +13,26 @@ namespace api.Services
         {
             _repository = repository;
             _logger = logger;
+        }
+
+        public IEnumerable<User> GetAllUser(bool trackChanges)
+        {
+            try
+            {
+                var companies =
+                _repository.Company.GetAllUser(trackChanges);
+            return companies;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetAllUser)} service method {ex}");
+            throw;
+            }
+        }
+
+        public object GetAllUsers(bool trackChanges)
+        {
+            throw new NotImplementedException();
         }
     }
 }
